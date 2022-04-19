c5507a4e3aba46aef3d57281e9c9e7b8fdbc80ef
substack <substack@bits.coop>
2022-04-19 13:40:31 -1000

    edge index spec

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ var pclip = require('./lib/algorithms.js').polygonClipping
 var earcut = require('earcut')
 var varint = require('varint')
 var parse = require('./lib/parse.js')
+var edgePack = require('./lib/edge.js')
 var getEdges = require('./lib/get-edges.js')
 var meshToCoords = require('./lib/mesh-to-coords.js')
 var fix = require('./lib/fix.js')
@@ -34,24 +35,16 @@ module.exports = function clip(A, B, opts) {
       for (var j = 0; j < clipped[i].length; j++) {
         var l = clipped[i][j].length
         var nn = null
+        var estart = positions.length/2
+        var es = []
         for (var k = 0; k < l; k++) {
           var n = clipped[i][j][k]
-          /*
-          positions.push(n.point[0], n.point[1])
-          if (nn !== null && (!n.intersect || !nn.intersect)) {
-            var e = positions.length/2
-            edges.push(e-2,e-1)
-          }
-          */
+          es.push(positions.length/2)
           positions.push(n[0], n[1])
-          var e = positions.length/2
-          edges.push(e-2,e-1)
           nn = n
         }
-        if (!clipped[i][j][0].intersect || !n.intersect) {
-          var e = positions.length/2
-          edges.push(e-1,e-l)
-        }
+        es.push(estart/2)
+        edges.push(es)
         if (j+1 !== clipped[i].length) holes.push(positions.length/2)
       }
       out.push(repackArea(buf, area, edges, positions, holes))
@@ -63,45 +56,20 @@ module.exports = function clip(A, B, opts) {
   }
 }
 
-function repackArea(buf, area, edges, positions, holes) {
+function repackArea(buf, area, edgeRuns, positions, holes) {
+  var edges = edgePack.pack(edgeRuns)
   var cells = earcut(positions, holes)
   var size = buf.length - (area.end - area.start)
     + varint.encodingLength(positions.length/2)
     + positions.length*4
     + varint.encodingLength(cells.length/3)
+    + varint.encodingLength(edges.length)
   for (var i = 0; i < cells.length; i++) {
     size += varint.encodingLength(cells[i])
   }
-  var edgeCount = 0
-  var start = -1, end = -1
-  for (var i = 0; i < edges.length; i+=2) {
-    var e0 = edges[i+0]
-    var e1 = edges[i+1]
-    if (e1 !== e0 + 1) {
-      if (end - start === 1 < start) { // pair
-        size += varint.encodingLength(start*2+0)
-        size += varint.encodingLength(end)
-      } else { // window
-        size += varint.encodingLength(start*2+1)
-        size += varint.encodingLength(end-start)
-      }
-      edgeCount++
-      start = -1
-    }
-    if (start < 0) start = e0
-    end = e1
+  for (var i = 0; i < edges.length; i++) {
+    size += varint.encodingLength(edges[i])
   }
-  if (edges.length > 0) {
-    if (end - start === 1 || end < start) { // pair
-      size += varint.encodingLength(start*2+0)
-      size += varint.encodingLength(end)
-    } else { // window
-      size += varint.encodingLength(start*2+1)
-      size += varint.encodingLength(end-start)
-    }
-    edgeCount++
-  }
-  size += varint.encodingLength(edgeCount)
 
   var nbuf = Buffer.alloc(size)
   buf.copy(nbuf, 0, 0, area.start)
@@ -119,41 +87,11 @@ function repackArea(buf, area, edges, positions, holes) {
     varint.encode(cells[i], nbuf, offset)
     offset += varint.encode.bytes
   }
-  varint.encode(edgeCount, nbuf, offset)
+  varint.encode(edges.length, nbuf, offset)
   offset += varint.encode.bytes
-  var start = -1, end = -1
-  for (var i = 0; i < edges.length; i+=2) {
-    var e0 = edges[i+0]
-    var e1 = edges[i+1]
-    if (e1 !== e0 + 1) {
-      if (end - start === 1) { // pair
-        varint.encode(start*2+0, nbuf, offset)
-        offset += varint.encode.bytes
-        varint.encode(end, nbuf, offset)
-        offset += varint.encode.bytes
-      } else { // window
-        varint.encode(start*2+1, nbuf, offset)
-        offset += varint.encode.bytes
-        varint.encode(end-start, nbuf, offset)
-        offset += varint.encode.bytes
-      }
-      start = -1
-    }
-    if (start < 0) start = e0
-    end = e1
-  }
-  if (edges.length > 0) {
-    if (end - start === 1 || end < start) { // pair
-      varint.encode(start*2+0, nbuf, offset)
-      offset += varint.encode.bytes
-      varint.encode(end, nbuf, offset)
-      offset += varint.encode.bytes
-    } else { // window
-      varint.encode(start*2+1, nbuf, offset)
-      offset += varint.encode.bytes
-      varint.encode(end-start, nbuf, offset)
-      offset += varint.encode.bytes
-    }
+  for (var i = 0; i < edges.length; i++) {
+    varint.encode(edges[i], nbuf, offset)
+    offset += varint.encode.bytes
   }
   buf.copy(nbuf, offset, area.end, buf.length)
   offset += buf.length - area.end
